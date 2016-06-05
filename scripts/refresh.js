1ccc9080d74b5d10751de33713e28b61f5f58dc8
Savant Krishna <savant.krishna@aba-systems.com.au>
2016-06-05 11:45:03 +1000

    refresh token script

---
+++ scripts/refresh.js
@@ -0,0 +1,30 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+var youtube = require('../library/googleyoutube');
+var youtube = new youtube();
+fs = require('fs');
+
+
+
+var toBaseString = function(var_name, val) {
+  return 'exports.'+var_name+'="'+val+'";';
+}
+
+var printToJS = function (tokens, file) {
+  var toString = function(key){ return toBaseString(key, tokens[key]);}
+  string = toString('access_token') + '\n' + toString('refresh_token')
+  fs.writeFile(file, string);
+
+}
+
+var tokens = youtube.fetchTokenFromFile();
+console.log(tokens['access_token'])
+console.log(tokens['expiry_date'])
+//printToJS(tokens, 'token.js')
+printToJS(tokens, '/Users/savant/Sites/australia-4-dolphins/cfg/token.js');
+
+var new_tokens = youtube.refreshToken(tokens, function(){});
+if (new_tokens){
+  console.log(new_tokens)
+}
